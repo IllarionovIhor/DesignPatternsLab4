446932873a420cc0ef066b0ae32eb14dc24461b5
myduckperson <117379458+IllarionovIhor@users.noreply.github.com>
2026-02-28 17:56:38 +0200

    uploading lab4

---
+++ Mediator/ClassLibrary/Aircraft.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Aircraft
+    {
+        public string Name { get; private set; }
+
+        public Aircraft(string name)
+        {
+            Name = name;
+        }
+
+        public void Land()
+        {
+            Console.Write($"Aircraft {Name} is landing ");
+        }
+
+        public void TakeOff()
+        {
+            Console.Write($"Aircraft {Name} is taking off ");
+        }
+    }
+}
---
+++ Mediator/ClassLibrary/CommandCentre.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class CommandCentre
+    {
+        private List<Runway> _runways = new List<Runway>();
+        private List<Aircraft> _aircrafts = new List<Aircraft>();
+
+        public CommandCentre(Runway[] runways, Aircraft[] aircrafts)
+        {
+            this._runways.AddRange(runways);
+            this._aircrafts.AddRange(aircrafts);
+        }
+
+        public void RequestLand(Aircraft aircraft)
+        {
+            Runway freeRunway = _runways.FirstOrDefault(r => !r.IsBusy);
+            if (freeRunway != null)
+            {
+                aircraft.Land();
+                Console.WriteLine($"on Runway {freeRunway.Id}.");
+                freeRunway.IsBusy = true;
+                freeRunway.HighLightRed();
+            }
+            else
+            {
+                Console.WriteLine("No free runways available for landing.");
+            }
+        }
+
+        public void RequestTakeOff(Aircraft aircraft)
+        {
+            Runway occupiedRunway = _runways.FirstOrDefault(r => r.IsBusy);
+            if (occupiedRunway != null)
+            {
+                aircraft.TakeOff();
+                Console.WriteLine($"from Runway {occupiedRunway.Id}.");
+                occupiedRunway.IsBusy = false;
+                occupiedRunway.HighLightGreen();
+            }
+            else
+            {
+                Console.WriteLine("No aircraft on a runway ready for takeoff.");
+            }
+        }
+    }
+}
